741dd3b5b638d19ac958a7a31a427d3c467e66d2
Lain <hhichri60@gmail.com>
2025-02-05 04:07:50 +0100

    add pip template

---
+++ setup.py
@@ -0,0 +1,42 @@
+import setuptools
+import pathlib
+from setuptools import find_packages
+
+
+def get_version() -> str:
+    rel_path = "src/prama/__init__.py"
+    with open(rel_path, "r") as fp:
+        for line in fp.read().splitlines():
+            if line.startswith("__version__"):
+                delim = '"' if '"' in line else "'"
+                return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = [
+        req
+        for req in f.read().splitlines()
+        if req and not (req.startswith("#") or req.strip() is None)
+    ]
+
+
+setuptools.setup(
+    name="prama",
+    version=get_version(),
+    author="person",
+    author_email="email@gmail.com",
+    description="A package for prama",
+    long_description=pathlib.Path("README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/PramaLLC/BEN2",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=requirements,
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
